7cc83cb964401a30dba552429467680e83b0cbf7
Serendipity <jacksparrow414@163.com>
2022-11-15 21:33:40 +0800

    update log4j2.xml

---
+++ src/test/java/com/example/log4j2/Log4j2ScriptTest.java
@@ -0,0 +1,15 @@
+package com.example.log4j2;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jacksparrow414
+ * @date 2022/11/15
+ */
+public class Log4j2ScriptTest {
+    
+    @Test
+    public void assertScript() {
+        String property = System.getProperty("os.name");
+    }
+}
