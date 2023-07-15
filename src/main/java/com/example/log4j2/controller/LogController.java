b2b15e5273ee7d8226cb47539245947772001153
Serendipity <jacksparrow414@163.com>
2023-07-15 10:31:17 +0800

    conditionally add log4j2 socket and null appender depending on java system property

--- src/main/java/com/example/log4j2/controller/LogController.java
+++ src/main/java/com/example/log4j2/controller/LogController.java
@@ -2,6 +2,9 @@ package com.example.log4j2.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.tomcat.util.net.AprEndpoint.Sendfile;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +21,20 @@ public class LogController {
     
     @GetMapping(value = "log")
     public void log() {
-        LOGGER.debug("Debug level log message");
-        LOGGER.info("Info level log message");
-        LOGGER.warn("Warn level log message");
-        LOGGER.error("Error level log message");
+//        LOGGER.debug("Debug level log message");
+//        LOGGER.info("Info level log message");
+//        LOGGER.warn("Warn level log message");
+//        LOGGER.error("Error level log message");
+        ThreadContext.put("clientIp", "127.0.0.1");
+        LOGGER.info("this is iner log");
+        ThreadContext.clearAll();
+    }
+    
+    @GetMapping(value = "logp")
+    public void logP() {
+        ThreadContext.put("clientIp", "192.168.100.12");
+        LOGGER.info("this is private log");
+        ThreadContext.clearAll();
+        
     }
 }
--- src/test/java/com/example/log4j2/Log4j2ScriptTest.java
+++ src/test/java/com/example/log4j2/Log4j2ScriptTest.java
@@ -1,5 +1,7 @@
 package com.example.log4j2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,5 +13,20 @@ public class Log4j2ScriptTest {
     @Test
     public void assertScript() {
         String property = System.getProperty("os.name");
+    }
+    
+    @Test
+    public void assertRegex() {
+        String content = "This is the text to be searched ";
+        String pattern = "(?<abc>text)";
+        // https://www.cnblogs.com/mengw/p/13531008.html
+        boolean isMatch = Pattern.compile(pattern).matcher(content).find();
+        // https://www.runoob.com/w3cnote/java-capture-group.html
+        Pattern compile = Pattern.compile(pattern);
+        Matcher matcher = compile.matcher(content);
+        matcher.find();
+        String group = matcher.group(0);
+        System.out.println(group);
+        System.out.println("字符串中是否包含了 'runoob' 子字符串? " + isMatch);
     }
 }
